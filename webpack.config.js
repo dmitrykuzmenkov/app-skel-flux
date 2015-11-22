3f7777b62b069cd5aa146d13c213cd74b6e84d0e
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-11-22 11:56:59 +0700

    Sepate env build for webpack. ENV=production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,57 @@ var ExtractText = require('extract-text-webpack-plugin');
 var CopyFile = require('copy-webpack-plugin');
 var LessClean = require('less-plugin-clean-css');
 var HtmlFile = require('html-webpack-plugin');
+var webpack = require('webpack');
 
+var config = {
+  dev: {
+    html: {},
+    loaders: {
+      less: 'css!autoprefixer?browsers=last 5 version' +
+        '!less?config=lessLoaderCustom'
+    },
+    plugins: []
+  },
+
+  production: {
+    devtool: 'source-map',
+    html: {
+      minify: {
+        collapseWhitespace: true,
+        removeScriptTypeAttributes: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyCSS: true,
+      }
+    },
+    loaders: {
+      less: 'css?sourceMap' +
+        '!autoprefixer?browsers=last 5 version' +
+        '!less?sourceMap=true&config=lessLoaderCustom'
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  }
+}[process.env.ENV || 'dev'];
+
+console.log(config);
 module.exports = {
   cache: true,
+  devtool: config.devtool,
   entry: [
     './main.js',
     './main.less'
   ],
   output: {
     path: 'build',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    pathinfo: false
   },
-  devtool: "eval-source-map",
   module: {
     loaders: [
       {
@@ -24,11 +63,7 @@ module.exports = {
         loader: 'url?limit=8192&name=asset/[name].[ext]'
       }, {
         test: /\.less$/,
-        loader: ExtractText.extract(
-          'css?sourceMap' +
-          '!autoprefixer?browsers=last 5 version' +
-          '!less?sourceMap=true&config=lessLoaderCustom'
-        )
+        loader: ExtractText.extract(config.loaders.less)
       }
     ]
   },
@@ -37,23 +72,19 @@ module.exports = {
       new LessClean({advanced: true})
     ]
   },
-  plugins: [
+  plugins: Object.assign([
+    new webpack.PrefetchPlugin('jext'),
+    new webpack.PrefetchPlugin('edispatcher'),
+    new webpack.PrefetchPlugin('domd'),
     new ExtractText('bundle.css'),
-    new HtmlFile({
+    new HtmlFile(Object.assign({
       filename: 'index.html',
       template: 'index.html',
       hash: true,
-      inject: 'head',
-      minify: {
-        collapseWhitespace: true,
-        removeScriptTypeAttributes: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        minifyCSS: true,
-      }
-    })
+      inject: 'head'
+    }, config.html))
     // new CopyFile([
     //   { from: 'web/static/assets/robots.txt' }
     // ])
-  ]
+  ], config.plugins)
 };
