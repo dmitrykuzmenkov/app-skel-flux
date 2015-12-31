97d7f7794a8feac2af9944b11a23b4c435a84573
Dmitry Kuzmenkov <dmitry@wagh.ru>
2015-12-31 18:17:54 +0700

    Back to component description in html cuz of need custom mount point for better dom delegate

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 require('./main.less');
 var dispatcher = require('edispatcher');
-var components = require('./config/component.config.js');
 
 document.addEventListener('DOMContentLoaded', function() {
-  components.forEach(function (component) {
+  var components = document.querySelectorAll('[component]');
+  Array.prototype.forEach.call(components, function (mount_point) {
+    var cn = mount_point.getAttribute('component');
     try {
-      var cc = require('./component/' + component + '/' + component + '.js');
-      new cc(document.body);
+      var cc = require('./component/' + cn + '/' + cn + '.js');
+      new cc(mount_point);
     } catch (e) {
-      console.warn('Failed to initialize component ' + component + '. ' + e);
+      console.warn('Failed to initialize component ' + cn + '. ' + e);
     }
   });
   dispatcher.send('components_loaded', null, 'main');
